568c78c73cb475c247ad3ceaa489c700fbd6698e
城北吕公 <878636660@qq.com>
2024-07-13 15:59:15 +0800

    集成支付宝沙箱支付

--- src/main/java/com/lvchenglong/controller/AliPayController.java
+++ src/main/java/com/lvchenglong/controller/AliPayController.java
@@ -21,6 +21,7 @@ import com.lvchenglong.entity.Orders;
 import com.lvchenglong.exception.CustomException;
 import com.lvchenglong.service.OrdersService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
